f8fca331390b2b2089276fb65670aaff5776e4ef
rebecarivas <rebecarivas@gmail.com>
2022-01-25 15:23:03 -0300

    feature: adiciona a funcionalidadehandleToggleTaskCompletion

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -23,19 +23,22 @@ export function TaskList() {
       isComplete: false
     }
     setTasks(tasks => [...tasks, newTask])
-    setNewTaskTitle('')
-    
+    setNewTaskTitle('') 
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskCompleted = tasks.map(task => task.id === id ? {
+      ...task, isComplete: !task.isComplete
+    } : task)
+    setTasks(taskCompleted)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
     const tasksFiltered = tasks.filter(task => task.id !== id)
     setTasks(tasksFiltered)
-  
+  }
   return (
     <section className="task-list container">
       <header>
